91bd7bda741af2a0cfe49442880d193abe2cc209
LessnessRandomness <lessnessr@gmail.com>
2025-01-12 18:47:09 +0200

    Fixed definition of Chain

--- Jordan/Basic.lean
+++ Jordan/Basic.lean
@@ -2,27 +2,34 @@ import Mathlib
 import ImportGraph.Imports
 
 -- definition of Chain and Polygon
+
+def get_segment (points: List (ℝ × ℝ)) (n: Fin (points.length - 1)): Set (ℝ × ℝ) :=
+  segment (𝕜 := ℝ × ℝ) (points[n.1]'(by omega)) (points[n.1 + 1]'(by omega))
+
 structure Chain where
   points: List (ℝ × ℝ)
   min_size: 2 ≤ points.length
-  get_segment (n: Fin (points.length - 1)): Set (ℝ × ℝ) :=
-    segment (𝕜 := ℝ × ℝ) (points[n.1]'(by omega)) (points[n.1 + 1]'(by omega))
   property_of_consecutive_segments: ∀ n m, n.1 + 1 = m.1 →
-    (get_segment n) ∩ (get_segment m) = { points[m.1]'(by omega) }
+    (get_segment points n) ∩ (get_segment points m) = { points[m.1]'(by omega) }
   nonconsecutive_segments_disjoint:
-    ∀ n m, n.1 + 1 < m.1 → Disjoint (get_segment n) (get_segment m)
+    ∀ n m, n.1 + 1 < m.1 → Disjoint (get_segment points n) (get_segment points m)
 
 def Chain.first (c: Chain): ℝ × ℝ := c.points[0]'(by cases c; simp at *; omega)
 def Chain.last (c: Chain): ℝ × ℝ := c.points[c.points.length - 1]'(by cases c; simp at *; omega)
 def Chain.connects (c: Chain) (x y: ℝ × ℝ): Prop := c.first = x ∧ c.last = y
-def Chain.as_Set (c: Chain): Set (ℝ × ℝ) := λ p => ∃ n, p ∈ c.get_segment n
+def Chain.as_Set (c: Chain): Set (ℝ × ℝ) := λ p => ∃ n, p ∈ get_segment c.points n
 
-structure Polygon where
+structure Polygon extends Chain where
   chain: Chain
-  can_be_closed: ∀ n, Disjoint (chain.get_segment n) (openSegment (𝕜 := ℝ × ℝ) chain.first chain.last)
+  can_be_closed: ∀ n, Disjoint (get_segment chain.points n) (openSegment (𝕜 := ℝ × ℝ) chain.first chain.last)
 
 def Polygon.as_Set (p: Polygon) := p.chain.as_Set ∪ segment (𝕜 := ℝ × ℝ) p.chain.first p.chain.last
 
+def example_of_chain: Chain :=
+  Chain.mk [(1, 2), (3, 4)]
+  (by simp)
+  (by simp)
+  (by simp)
 
 
 --- place for useful stuff, sorried lemmas etc. (for now)
@@ -37,16 +44,10 @@ noncomputable def right_cross (q r s: ℝ × ℝ): ℝ :=
   then if (q.2 ≤ s.2) ∨ (r.2 < q.2) then 1 else delta q s r
   else if (q.2 ≤ r.2) ∨ (s.2 < q.2) then 1 else delta q r s
 
-noncomputable def point_in_polygon_aux (L: List (ℝ × ℝ)) (HL: 2 ≤ L.length) (Lf: List (Fin (L.length))) (q: ℝ × ℝ): ℝ :=
-  match Lf with
-  | .nil => -1
-  | x :: t => right_cross q (L[x.1]'(x.2)) (L[x.add ⟨1, by omega⟩]'(by simp)) * point_in_polygon_aux L HL t q
-
 noncomputable def point_in_polygon (p: Polygon) (q: ℝ × ℝ): ℝ :=
   let ps := p.chain.points
-  point_in_polygon_aux ps p.chain.min_size (List.finRange ps.length) q
-
-
+  let one: Fin ps.length := ⟨1, by have H := p.chain.min_size; unfold ps; omega⟩
+  (- ∏ (n: Fin (ps.length)), right_cross q (ps[n.1]'(n.2)) (ps[n.add one]'(by simp)))
 
 
 --- statement of baby Jordan
@@ -68,7 +69,6 @@ theorem Jordan_curve_theorem (jc: Jordan_curve):
   ∃ A B, (Set.range jc.param)ᶜ = A ∪ B ∧
   A.Nonempty ∧ B.Nonempty ∧ Disjoint A B ∧
   IsConnected A ∧ IsConnected B ∧ IsOpen A ∧ IsOpen B := sorry
-
 
 
 
